0f7d8741f3375b952adfd41206af343ff6ee9048
Hendrew Barreto <hendrew.dev@gmail.com>
2026-06-21 09:34:20 -0300

    test: user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name                  { Faker::Name.name }
+    active                { true }
+    email                 { Faker::Internet.email }
+    password              { "123456" }
+    password_confirmation { "123456" }
+  end
+end
